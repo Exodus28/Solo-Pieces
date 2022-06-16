b781e32d19f3a99e2ca089152d4b87cebd79ac45
Chris <43829555+Exodus28@users.noreply.github.com>
2022-06-16 09:10:30 +0100

    Add files via upload
    Added comments & eventlistener to use Enter key on text field

--- Lead Tracker Chrome Extension/index.js
+++ Lead Tracker Chrome Extension/index.js
@@ -1,36 +1,37 @@
-let myLeads = []
-let checkBoxes = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+let myLeads = []            //empty leads array
+let leadsRemaining          //remaining leads variable to be passed leads array after filter
+let itemsToRemove = []      //empty array to be filled with leads to remove based on filter
+
+const inputEl = document.getElementById("input-el") //text input field
+const inputBtn = document.getElementById("input-btn") //input button
+
+const ulEl = document.getElementById("ul-el")   //unordered list element
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )//get local storage
+
+const deleteBtn = document.getElementById("delete-btn") //delete, save tab, select buttons
 const tabBtn = document.getElementById("tab-btn")
 const selectBtn = document.getElementById("select-btn")
 const selectAllBox = document.getElementById("check0")
 
-let leadsRemaining
-let itemsToRemove = []
-
-const testBtn = document.getElementById("test-btn")
-
-if (leadsFromLocalStorage) {
+if (leadsFromLocalStorage) {            //if there is localStorage used, populate myLeads array
     myLeads = leadsFromLocalStorage
-    render(myLeads)
+    render(myLeads)                     //call render function, passing in myLeads array
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
+tabBtn.addEventListener("click", function(){    //on click of tab button call function
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){ //check active tab/window
+        myLeads.push(tabs[0].url)                   //push tab url to myLeads array
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) ) //set array to local storage
+        render(myLeads)                         //call render function
     })
 })
 
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
+function render(leads) {        //draw items to page
+    let listItems = ""          //set empty string
+    for (let i = 0; i < leads.length; i++) {        //use ` ` to input code within string -
+                                                    //create list item, checkbox & set IDs,
+                                                    //label, and hyperlink to myLeads item
+        listItems += `          
             <li>
                 <input type="checkbox" class="checkBox, check" id="cb-${[i]}">
                     <label>${[i + 1]}
@@ -40,45 +41,66 @@ function render(leads) {
             </li>
         `
     }
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems  //update unordered list with list items
 }
 
-deleteBtn.addEventListener("click", function() {
-    for (let i = 0; i < myLeads.length; i++){
-        tickBox = document.getElementById(`cb-${[i]}`)
-        if(tickBox.checked == true){
-            itemsToRemove.push(`${myLeads[i]}`)
-            console.log(itemsToRemove)
+deleteBtn.addEventListener("click", function() {    //onclick of delete button
+    for (let i = 0; i < myLeads.length; i++){       //for each item in myLeads
+        tickBox = document.getElementById(`cb-${[i]}`)  //if tickBox ID (set via listItems)
+        if(tickBox.checked == true){                    //is checked
+            itemsToRemove.push(`${myLeads[i]}`)         //push myLeads index item to empty array
+            
+            //console.log(itemsToRemove)
         }
-            leadsRemaining = myLeads.filter(x => !itemsToRemove.includes(x));
-            console.log(leadsRemaining)         
+            leadsRemaining = myLeads.filter(x => !itemsToRemove.includes(x)); 
+            /* populate leadsRemaining variable with result(s) of filtering any items in myLeads
+            which do NOT include those now stored in itemsToRemove array 
+            
+            N.B. as of time of writing I do not fully understand arrow functions YET, however 
+            this was the only method I could successfully implement on researching removal of
+            specific array items. Had previously attemped (many, many times) splice with some
+            success but ran into issues with deleting multiple entries and this having a knock-on
+            effect on the 'for' loop & the indexing of items within the array(s) */
+            
+            //console.log(leadsRemaining)         
     }
-    myLeads = leadsRemaining
-    localStorage.clear()
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    myLeads = leadsRemaining //update myLeads array to adopt value of leadsRemaining
+    itemsToRemove = []  //clear itemsToRemove array to avoid accidental deletions in future
+    localStorage.clear() //clear localStorage so will be no duplication
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) //set localStorage to myLeads 
+    render(myLeads) //render again to update results drawn to page
 })
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
+inputBtn.addEventListener("click", function() { //input button click
+    myLeads.push(inputEl.value)                 //push input element value to myLeads array
+    inputEl.value = ""                          //set input element value back to blank string
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )   //add to localStorage
+    render(myLeads)                             //render
 })
 
-selectBtn.addEventListener("click", function(){
-    if(selectAllBox.checked == true){
-        selectAllBox.checked = false
-        for(let i = 0; i < myLeads.length; i++){
-            tickBox = document.getElementById(`cb-${[i]}`).checked = false
+inputEl.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard and the input field is NOT blank
+  if (event.key === "Enter" && inputEl.value != "") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    inputBtn.click();
+  }
+});
+
+selectBtn.addEventListener("click", function(){ //select All button click
+    if(selectAllBox.checked == true){       //if checked
+        selectAllBox.checked = false        //set to false
+        for(let i = 0; i < myLeads.length; i++){ //for all items in myLeads
+            tickBox = document.getElementById(`cb-${[i]}`).checked = false  //set checked to false
         }
     }
-    else if(selectAllBox.checked == false){
-        selectAllBox.checked = true
-        for(let i = 0; i < myLeads.length; i++){
-            tickBox = document.getElementById(`cb-${[i]}`).checked = true
+    else if(selectAllBox.checked == false){ //if not checked
+        selectAllBox.checked = true         //set to true
+        for(let i = 0; i < myLeads.length; i++){ //for all in array
+            tickBox = document.getElementById(`cb-${[i]}`).checked = true //set checked to true
         }
     }
     else
-    console.log("none to check")
+    console.log("none to check") //should never fire but just in case, y'know
 })
